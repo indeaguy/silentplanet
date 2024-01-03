d7e2531b14a7cc9c656c4c28f44b880e672ac47e
indeaguy <davide.mcguigan@gmail.com>
2024-01-03 13:32:19 -0400

    Fixing raytracing.

--- static/js/controllers/App.js
+++ static/js/controllers/App.js
@@ -18,14 +18,15 @@ export class App {
     this.renderer.renderables.push(this.globe);
 
     const loader = new DataLoader('geojson/usa.geojson');
-    
+
     loader.loadData().then(data => {
       const result = loader.mapDataToSphere(data, config.SPHERE.RADIUS, config.POLYGONS.COLOR, config.POLYGONS.RISE, config.POLYGONS.SUBDIVIDE_DEPTH, config.POLYGONS.MIN_EDGE_LENGTH);
       result.meshes.forEach(mesh => this.renderer.scene.add(mesh));
     });
 
     // a custom raycaster handler
-    this.rayTracer = new RayTracer(this.renderer.scene, this.renderer.camera);
+    this.hoverRayTracer = new RayTracer(this.renderer.scene, this.renderer.camera, 0x00ff00, 0xFFC0CB);
+    this.clickRayTracer = new RayTracer(this.renderer.scene, this.renderer.camera, 0x00ffff, 0xFFC000);
 
     this.renderer.animate();
 
@@ -34,7 +35,8 @@ export class App {
 
   setupEventListeners() {
     window.addEventListener('resize', () => this.renderer.onWindowResize(), false);
-    window.addEventListener('mousemove', (event) => this.rayTracer.onMouseMove(event), false);
+    window.addEventListener('mousemove', (event) => this.hoverRayTracer.handleRayEvent(event), false);
+    window.addEventListener('click', (event) => this.clickRayTracer.handleRayEvent(event), false);
   }
 
 }
--- static/js/models/base/RayTracer.js
+++ static/js/models/base/RayTracer.js
@@ -1,38 +1,19 @@
 import * as THREE from 'three'; // Import the earcut library for triangulation.
 
 export class RayTracer {
-  constructor(scene, camera, hoverColor = 0xFFC0CB, originalColor = 0x00ff00) {
+  constructor(scene, camera, originalColour = 0x00ff00, eventColour = 0xFFC0CB) {
     this.scene = scene;
     this.camera = camera;
-    this.hoverColor = new THREE.Color(hoverColor);
-    this.originalColor = new THREE.Color(originalColor);
+    this.originalColour = new THREE.Color(originalColour);
+    this.eventColour = new THREE.Color(eventColour);
     this.mouse = new THREE.Vector2();
     this.raycaster = new THREE.Raycaster();
-    this.intersected = null;
-
     this.plane = new THREE.Plane();
     this.planeNormal = new THREE.Vector3();
-
-    // @TODO should only do this when the appropriate debugging is enabled
-    // A circle indicating where the ray is going
-    const circleGeometry = new THREE.CircleGeometry(0.5, 32); // Small circle with a radius of 0.5
-    const circleMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
-    this.indicator = new THREE.Mesh(circleGeometry, circleMaterial);
-    this.indicator.visible = false; // Initially hidden
-    scene.add(this.indicator);
-
-     // @TODO should only do this when the appropriate debugging is enabled
-    // a dot indicating where the sphere was intersected
-    const dotGeometry = new THREE.SphereGeometry(0.1, 32, 32); // Small sphere geometry
-    const dotMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Red color
-    this.indicatorDot = new THREE.Mesh(dotGeometry, dotMaterial);
-    this.indicatorDot.visible = false; // Initially hidden
-    scene.add(this.indicatorDot);
-
-    
+    this.intersected = null;
   }
 
-  onMouseMove(event) {
+  handleRayEvent(event) {
     this.mouse.x = (event.offsetX / window.innerWidth) * 2 - 1;
     this.mouse.y = - (event.offsetY / window.innerHeight) * 2 + 1;
     this.checkIntersections();
@@ -53,17 +34,14 @@ export class RayTracer {
         console.log('Intersected object name:', firstNamedIntersect.object.name);
 
         if (this.intersected !== firstNamedIntersect.object) {
-            if (this.intersected) this.intersected.material.color.set(this.originalColor);
+            if (this.intersected) this.intersected.material.color.set(this.originalColour);
             this.intersected = firstNamedIntersect.object;
-            this.intersected.material.color.set(this.hoverColor);
-
-            this.indicatorDot.position.copy(firstNamedIntersect.point);
-            this.indicatorDot.visible = true;
+            this.intersected.material.color.set(this.eventColour);
         }
     } else {
-        if (this.intersected) this.intersected.material.color.set(this.originalColor);
+        if (this.intersected) this.intersected.material.color.set(this.originalColour);
+
         this.intersected = null;
-        this.indicatorDot.visible = false;
     }
   }
 
